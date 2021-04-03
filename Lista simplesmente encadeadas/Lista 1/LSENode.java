d5991a7a379a33d84dab78985de909c8fb61ed05
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-03 01:22:09 -0300

    O código segue essas exigências, abaixo:
    Questão única: Implemente um programa para ler números inteiros digitados pelo usuário e armazená-los
    em duas listas (A e B), segundo a seguinte regra: a lista A deverá armazenar apenas números pares e a lista
    B apenas números ímpares. O programa deverá ter os seguintes métodos:
    
    (a) Um método de inserção de valores que receba como parâmetro o valor a ser inserido. A inserção do
    novo valor deve ocorrer sempre um final da lista. Pré-requisito para a inserção: o valor a ser
    inserido não pode ser repetido.
    
    (b) Um método de busca. Uma função que recebe como parâmetro o valor a ser procurado. A função
    deve retornar true, caso encontre o valor procurado, ou false, caso o valor procurado não seja
    encontrado. Utilize esta função de busca na implementação do item (a).
    
    (c) Um método para exibir os valores armazenados em uma lista.
    O programa deverá ter um menu (vide abaixo) que permitirá ao usuário executar as opções de inserção,
    busca e exibição a qualquer momento e quantas vezes desejar.
    
    Menu de opções:
    1 – Armazenar número
    2 – Procurar número
    3 – Exibir números pares
    4 – Exibir números ímpares
    5 – Encerrar programa
    Digite a opção desejada:

---
+++ Lista simplesmente encadeadas/Lista 1/LSENode.java
@@ -0,0 +1,25 @@
+package br.unicap.c3.ed1.lista1;
+/*
+ * @author valmito
+ */
+public class LSENode<T extends Comparable <T>>{//usado para comparar objetos
+    private T info;
+    private LSENode<T> prox;
+    
+    LSENode(T valor){
+        info=valor;
+    }
+    void setInfo(T valor){
+        info= valor;
+    }
+    T getInfo(){
+        return info;
+    }
+    void setProx(LSENode<T>novoProx){
+        prox= novoProx;
+    }
+    LSENode<T> getProx(){
+        return prox;
+    }
+}
+    
