bf52363877485e6c7552fc05ee81255d32d84e83
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-04-12 10:50:04 -0300

    Create Node.java

---
+++ LDE - Lista duplamente encadeada/Atividade 4 - Questão 1/Node.java
@@ -0,0 +1,29 @@
+package lista4q1;
+public class Node  {
+    
+    private Node ant;
+    private Cliente info;
+    private Node prox;
+    
+    public Node(Cliente c){
+        this.info=c;
+    }
+    public void setAnt(Node novoAnt){
+        this.ant=novoAnt;
+    }
+    public void setProx(Node novoProx){
+        this.prox=novoProx;
+    }
+    public void setInfo(Cliente c){
+        this.info=c;
+    }
+    public Node getAnt(){
+        return this.ant;
+    }
+    public Node getProx(){
+        return this.prox;
+    }
+    public Cliente getInfo(){
+        return this.info;
+    }
+}
