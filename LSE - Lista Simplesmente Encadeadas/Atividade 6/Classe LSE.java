0aa7b2a0dec5ae85824cdfc5e49e195e2e8767f0
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-05-12 14:31:58 -0300

    Create Classe LSE.java

---
+++ LSE - Lista Simplesmente Encadeadas/Atividade 6/Classe LSE.java
@@ -0,0 +1,65 @@
+package atividade6;
+public class LSE {
+    private Node ultimo;
+    private Node primeiro;
+    private int quantidade;
+    
+    public boolean isEmpty(){
+        if(this.primeiro==null && this.ultimo==null && this.quantidade==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void inserirInicio(Integer valor){
+        Node novo= new Node(valor);
+        if(this.isEmpty()==true){
+            System.out.println("Lista vázia!");
+            this.primeiro=novo;
+            this.ultimo=novo;
+            this.quantidade=0;
+            System.out.println("Inserção efetuada!");
+        }else{
+            novo.setProximo(this.primeiro);
+            this.primeiro=novo;
+            this.quantidade++;
+            System.out.println("Inserção efetuada!");
+        }
+    }
+    public void exibir(){
+        Node aux;
+        if(this.isEmpty()==true){
+            System.out.println("Lista vázia!");
+        }else{
+            aux= this.primeiro;
+            while(aux!=null){
+                System.out.println(aux.getInformacao());
+                aux=aux.getProximo();
+            }
+        }
+    }
+    public LSE intersecao(LSE lista2){
+        LSE lista3= new LSE();
+        int contador=0;
+        Integer valor1,valor2,valor3;
+        if(this.isEmpty()==true && lista2.isEmpty()==true){
+            System.out.println("Lista vázia");
+            return lista3;
+        }else{
+            Node aux1= this.primeiro;
+            Node aux2= lista2.primeiro;
+            while(aux1!= null && aux2!=null){
+                if(aux1.getInformacao().compareTo(aux2.getInformacao())==0){
+                    valor1= aux1.getInformacao();
+                    //valor3= new Integer(valor1);
+                    lista3.inserirInicio(valor1);
+                    contador= contador+1;
+                    System.out.println("Interpolação efetuada "+contador+" vezes.");
+                }
+                    aux1=aux1.getProximo();
+                    aux2=aux2.getProximo();
+                }
+            }
+        return lista3;
+    }
+}
