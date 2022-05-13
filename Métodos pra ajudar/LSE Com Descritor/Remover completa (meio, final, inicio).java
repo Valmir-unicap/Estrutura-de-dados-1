c5647d78b11d0c87c5ea0cf46f8bcd4d2f1ec772
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-05-13 12:31:17 -0300

    Update Remover completa (meio, final, inicio).java

--- Métodos pra ajudar/LSE Com Descritor/Remover completa (meio, final, inicio).java
+++ Métodos pra ajudar/LSE Com Descritor/Remover completa (meio, final, inicio).java
@@ -1,47 +1,61 @@
 //LSE - Nunca está ordenado
 //Não serve para ordenação
-public void Remover(String matr){//Remover na lista
-        Aluno proc = new Aluno(matr);
-        Node atual, anterior;
-        if(this.isEmpty()==true){//lista vázia
-            System.out.println("Lista Vázia!");
-        }else if(this.qtd==1){//remoção no inicio da lista. - só tem um elemento a lista
-                if(proc.equals(this.primeiro.getInfo())){
-                    this.primeiro=null;
-                    this.ultimo=null;
-                    this.qtd=0;
-                    System.out.println("Remoção efetuada! ");
-                    }else{
-                        System.out.println("Não encontrado!");
-                    }
-                //é o primeiro? - lista tem mais de um elemento
-                }else if(proc.equals(this.primeiro.getInfo())){
-                        this.primeiro= this.primeiro.getProx();
+//Orenação signfica ordem crescente ou descrescente
+public void remover (String matr) {
+        Aluno proc = new Aluno (matr);
+        Node anterior, atual;
+        boolean achou = false;
+        if (this.isEmpty() == true) {
+            System.out.println("Lista vazia");
+        }
+        else if (this.qtd == 1) {
+            if (proc.equals(this.primeiro.getInfo())) {
+                this.primeiro = null;
+                this.ultimo = null;
+                this.qtd = 0;
+                System.out.println("Removido!");
+            }
+            else {
+                System.out.println("Não encontrado!");
+            }    
+        }
+        else { // lista tem mais de um elemento
+            if (proc.equals(this.primeiro.getInfo()) == true) { // é o primeiro??????
+                this.primeiro = this.primeiro.getProx();
+                this.qtd--;
+                System.out.println("Removido!");
+            }
+            else if (proc.equals(this.ultimo.getInfo()) == true) { // é o último?????
+                anterior = this.primeiro;
+                while (anterior.getProx() != this.ultimo) {
+                    anterior = anterior.getProx();
+                }
+                anterior.setProx(null);
+                this.ultimo = anterior;
+                this.qtd--;
+                System.out.println("Removido!!!");
+            }
+            else { // é um do meio ??????
+                anterior = this.primeiro;
+                atual = anterior.getProx();
+                while (atual != null) {
+                    if (proc.equals(atual.getInfo()) == true) {
+                        anterior.setProx(atual.getProx());
                         this.qtd--;
-                        System.out.println("Remoção efetuada!");
-               //remoção no ultimo
-                }else if (proc.equals(this.ultimo.getInfo())){
-                            anterior= this.primeiro;
-                            while(anterior.getProx()!=this.ultimo){
-                                anterior= anterior.getProx();
-                            }
-                            anterior.setProx(null);
-                            this.ultimo= anterior;
-                            this.qtd--;
-                            System.out.println("Remoção efetuada!");
-                            }
-                //remoção no meio
-                    anterior= this.primeiro;
-                    atual= anterior.getProx();
-                    while(atual!=null){
-                        if(proc.equals(atual.getInfo())==true){
-                            anterior.setProx(atual.getProx());//
-                            this.qtd--;
-                            System.out.println("Remoção efetuada!");
-                            return;//interrompe o loop
-                        }else{
-                            atual= atual.getProx();
-                            anterior= anterior.getProx();
-                        }
+                        System.out.println("Removido!!!");
+                        achou = true;
+                        break;
+                    }
+                    else {
+                        atual = atual.getProx();
+                        anterior = anterior.getProx();
                     }
                 }
+                if (achou == false) {
+                    System.out.println("Não achei!!!!");
+                }
+            }
+        }
+       
+    }
+}
